ba0dda7a4b63d9600d6ccac3f669890433015253
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-01-22 14:27:51 -0300

    Initial Structure and Lint Config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'no-console': 'off'
+  },
+  'globals': {
+    'fetch': false,
+    'window': true
+  }
+}
---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => <View style={styles.container} />;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
+
+export default App;
