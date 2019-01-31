78920c847011f5f77089301435bbd9a739e46a1f
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-01-31 10:12:01 -0300

    New Route Add.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,10 +1,20 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './containers/HomeScreen';
+import SubjectDetailsScreen from './containers/SubjectDetailsScreen';
 import { colors } from './utils/Styles';
 
 const AppRouter = createStackNavigator({
   Main: {
     screen: HomeScreen,
+    navigationOptions: () => ({
+      headerStyle: {
+        backgroundColor: colors.primary.dark
+      },
+      headerTintColor: '#FFF'
+    }),
+  },
+  SubjectDetails: {
+    screen: SubjectDetailsScreen,
     navigationOptions: () => ({
       headerStyle: {
         backgroundColor: colors.primary.dark
