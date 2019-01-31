8f51316975ad5cc3a1fcf2e52ea860c9b193879f
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-01-31 10:14:01 -0300

    Router and Render Functions Creation.

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet, View
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import SubjectList from '../presentations/SubjectsList';
+import ButtonAdd from '../components/ButtonAdd';
+import CustomModal from '../components/CustomModal';
+import { colors } from '../utils/Styles';
 
 class HomeScreen extends Component {
   static navigationOptions = {
     title: 'PhotoClass'
   };
 
+  state = {
+    showNewSubjectModal: false
+  };
+
+  showNewSubjectModal = () => {
+    this.setState({
+      showNewSubjectModal: true
+    });
+  };
+
+  closeNewSubjectModal = () => {
+    this.setState({
+      showNewSubjectModal: false
+    });
+  };
+
   render() {
+    const { showNewSubjectModal } = this.state;
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <SubjectList />
+        <SubjectList
+          navigation={navigation}
+        />
+        <ButtonAdd showNewSubjectModal={this.showNewSubjectModal} />
+        {showNewSubjectModal === true && (
+          <CustomModal
+            close={this.closeNewSubjectModal}
+            title="Nova Matéria"
+            headerTextColor={colors.primary.dark}
+            content={null}
+          />
+        )}
       </View>
     );
   }
--- src/presentations/SubjectsList.js
+++ src/presentations/SubjectsList.js
@@ -1,71 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 import SubjectCard from './SubjectCard';
 import { colors, fonts } from '../utils/Styles';
 
-const goToSubjectDetails = () => {};
+class SubjectsList extends Component {
+  goToSubjectDetails = () => {
+    const { navigation } = this.props;
+    navigation.navigate('SubjectDetails');
+  };
 
-const SubjectsList = () => (
-  <View style={styles.mainView}>
-    <SubjectCard
-      isLoading
-      title="Matéria 1"
-      topOnPress={goToSubjectDetails}
-      bottomOnPress={goToSubjectDetails}
-      topColors={[colors.blue.dark]}
-      bottomColors={[colors.blue.dark, colors.blue.light]}
-      bottomContent={<View style={styles.homeCardBottomContent} />}
-      icon={(
-        <Icon
-          name="donut-large"
-          size={45}
-          color={colors.white}
-          style={{ backgroundColor: 'transparent' }}
+  render() {
+    return (
+      <View style={styles.mainView}>
+        <SubjectCard
+          isLoading
+          title="Matéria 1"
+          topOnPress={this.goToSubjectDetails}
+          bottomOnPress={this.goToSubjectDetails}
+          topColors={[colors.blue.dark]}
+          bottomColors={[colors.blue.dark, colors.blue.light]}
+          bottomContent={<View style={styles.homeCardBottomContent} />}
+          icon={(
+            <Icon
+              name="donut-large"
+              size={45}
+              color={colors.white}
+              style={{ backgroundColor: 'transparent' }}
+            />
+)}
         />
+        <SubjectCard
+          isLoading
+          title="Matéria 2"
+          topOnPress={this.goToSubjectDetails}
+          bottomOnPress={this.goToSubjectDetails}
+          topColors={[colors.purple.dark]}
+          bottomColors={[colors.purple.dark, colors.purple.light]}
+          bottomContent={<View style={styles.homeCardBottomContent} />}
+          icon={(
+            <Icon2
+              name="barcode"
+              size={45}
+              color={colors.white}
+              style={{ backgroundColor: 'transparent' }}
+            />
 )}
-    />
-    <SubjectCard
-      isLoading
-      title="Matéria 2"
-      topOnPress={goToSubjectDetails}
-      bottomOnPress={goToSubjectDetails}
-      topColors={[colors.purple.dark]}
-      bottomColors={[colors.purple.dark, colors.purple.light]}
-      bottomContent={(
-        <View style={styles.homeCardBottomContent} />
-)}
-      icon={(
-        <Icon2
-          name="barcode"
-          size={45}
-          color={colors.white}
-          style={{ backgroundColor: 'transparent' }}
         />
+        <SubjectCard
+          isLoading
+          title="Matéria 3"
+          topOnPress={this.goToSubjectDetails}
+          bottomOnPress={this.goToSubjectDetails}
+          topColors={[colors.green.dark]}
+          bottomColors={[colors.green.dark, colors.green.light]}
+          bottomContent={<View style={styles.homeCardBottomContent} />}
+          icon={(
+            <Icon
+              name="attach-money"
+              size={45}
+              color={colors.white}
+              style={{ backgroundColor: 'transparent' }}
+            />
 )}
-    />
-    <SubjectCard
-      isLoading
-      title="Matéria 3"
-      topOnPress={goToSubjectDetails}
-      bottomOnPress={goToSubjectDetails}
-      topColors={[colors.green.dark]}
-      bottomColors={[colors.green.dark, colors.green.light]}
-      bottomContent={(
-        <View style={styles.homeCardBottomContent} />
-)}
-      icon={(
-        <Icon
-          name="attach-money"
-          size={45}
-          color={colors.white}
-          style={{ backgroundColor: 'transparent' }}
         />
-)}
-    />
-  </View>
-);
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   mainView: {
@@ -76,22 +79,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 15,
     alignItems: 'center'
-  },
-  fabContainer: {
-    backgroundColor: colors.primary.dark,
-    height: 55,
-    width: 55,
-    borderRadius: 30,
-    position: 'absolute',
-    right: 20,
-    bottom: 24
-  },
-  fabTouchable: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
   },
   homeCardBottomContent: {
     flexDirection: 'row',
